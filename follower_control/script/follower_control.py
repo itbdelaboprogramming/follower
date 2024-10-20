44e20c7aa802166bc1e43f0dc676b081e67f7680
goldwinsonick <gswt.code@gmail.com>
2024-10-21 00:21:57 +0700

    feat: modify convertion between target to motor speed with parameters (constant/proportional) (need tuning)

--- follower_control/script/follower_control.py
+++ follower_control/script/follower_control.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os, sys
 from typing import Union
+import math
 
 import rospy
 from ros_msd700_msgs.msg import HardwareCommand, HardwareState
@@ -10,47 +11,64 @@ from geometry_msgs.msg import Twist, PoseStamped
 
 class FollowerControl:
     def __init__(self):
+        self.const_speed = rospy.get_param("/follower_control/constant_speed", False)
+        self.max_fwd = rospy.get_param("/follower_control/max_fwd", 0.5)
+        self.max_rot = rospy.get_param("/follower_control/max_rot", 0.5)
+        self.kp_fwd = rospy.get_param("/follower_control/kp_fwd", 0.1)
+        self.kp_rot = rospy.get_param("/follower_control/kp_rot", 0.3)
+        self.err_fwd = rospy.get_param("/follower_control/err_fwd", 0.1)
+        self.err_rot = rospy.get_param("/follower_control/err_rot", 0.1)
+
         # Publisher/Subscriber
         self.hardware_command_pub = rospy.Publisher("/hardware_command", HardwareCommand, queue_size=10)
         self.uwb_target_sub = rospy.Subscriber("/follower/uwb_target", PoseStamped, self.uwb_target_callback, queue_size=10)
     
-    def uwb_target_callback(self, msg: PoseStamped):
-        left_rpm, right_rpm = self.target_to_motor_speed(msg)
+    def uwb_target_callback(self, msg: PoseStamped) -> None:
+        left_pwm, right_pwm = self.target_to_motor_pwm(msg)
         new_hw_cmd = HardwareCommand()
         new_hw_cmd.movement_command = 0
         new_hw_cmd.cam_angle_command = 0
-        new_hw_cmd.right_motor_speed = right_rpm
-        new_hw_cmd.left_motor_speed = left_rpm
+        new_hw_cmd.right_motor_speed = right_pwm * 100
+        new_hw_cmd.left_motor_speed = left_pwm * 100
         self.hardware_command_pub.publish(new_hw_cmd)
 
-    def target_to_motor_speed(self, target : PoseStamped) -> Union[int, int]:
+    def target_to_motor_pwm(self, target : PoseStamped) -> Union[int, int]:
         # Define target pos and ort
         target_pose = target.pose
         target_pos = target_pose.position
 
-        # These param should be in a yaml file and need tuning
-        max_speed = 1
-        max_turn = 0.8
-        wheel_radius = 2.75
-        wheel_distance = 23.0
-        err_y = 0.1
+        # x y is coordinate, i need plar
+        rho = (target_pos.x**2 + target_pos.y**2)**0.5
+        theta = math.atan2(target_pos.y, target_pos.x)
 
         # x is forward, y is left/right
-        fwd_speed = (max_speed*100.0/wheel_radius)*9.55
-        rot_speed = (max_turn*wheel_distance/(2.0*wheel_radius))*9.55
+        if not self.const_speed:
+            fwd_speed = min(self.max_fwd , abs(self.kp_fwd * rho))
+            rot_speed = min(self.max_rot , abs(self.kp_rot * theta))
+        else:
+            fwd_speed = self.max_fwd
+            rot_speed = self.max_rot
 
-        if(abs(target_pos.y) < err_y):
+        # Robot doesn't move
+        left_motor_pwm = 0
+        right_motor_pwm = 0
+        
+        # If robot facing target
+        if(abs(theta) < self.err_rot):
             # forward/backward movement
-            if(target_pos.x > 2):
-                left_motor_rpm = fwd_speed
-                right_motor_rpm = fwd_speed
+            if(abs(rho) > self.err_fwd):
+                direction = abs(rho) / rho
+                left_motor_pwm = fwd_speed
+                right_motor_pwm = fwd_speed
         else:
             # rotation movement
-            direction = 1 if target_pos.y > 0 else -1
-            left_motor_rpm = direction*rot_speed
-            right_motor_rpm = -direction*rot_speed
+            direction = abs(theta) / theta
+            left_motor_pwm = direction*rot_speed
+            right_motor_pwm = -direction*rot_speed
         
-        return left_motor_rpm, right_motor_rpm
+        rospy.loginfo(f"left_motor_pwm: {left_motor_pwm:.2f}, right_motor_pwm: {right_motor_pwm:.2f}")
+        
+        return left_motor_pwm, right_motor_pwm
     
 if __name__ == '__main__':
     rospy.init_node('follower_control')
